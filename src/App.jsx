8d3e9361f3f5b834b680327aee20313f61324df4
Rathin7 <rathinbaraiRB637@gmail.com>
2023-05-18 02:47:11 +0600

    Client site setup done

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div>
+      <h2 className='text-3xl text-indigo-600'>Welcome to babQueen site</h2>
+      <button className="btn btn-primary">Button</button>
+    </div>
+  );
+};
+
+export default App;
